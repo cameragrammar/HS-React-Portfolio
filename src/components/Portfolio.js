3011638c8ca6616b815a6c88e5989c9c1d6e8a23
Hans Shen <116024825+cameragrammar@users.noreply.github.com>
2023-04-07 13:49:38 -0600

    Add files via upload

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,14 +1,71 @@
 import React from "react";
+import NBAPlayerTrackerImage from "../components/NBAPlayerTracker.JPG";
+import WeatherDashboardImage from "../components/WeatherDashboard.JPG";
+import NoteTakerImage from "../components/NoteTaker.JPG";
 
 function Portfolio() {
   return (
     <div className="container">
       <div className="white-box">
-        <div className="intro-info">
-          <p className="portfolio">
+        <div
+          className="intro-info"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div className="portfolio-header">
             <h2>Portfolio</h2>
-            <br />
-          </p>
+          </div>
+        </div>
+        <br />
+        <div className="project-container">
+          <div className="project-item">
+            <a href="https://github.com/project-1">
+              <img
+                src={NBAPlayerTrackerImage}
+                alt="Project 1"
+                style={{ width: "200px", height: "200px" }}
+              />
+            </a>
+            <div className="project-text">
+              <p>NBA Player Tracker</p>
+              <a href="https://github.com/cameragrammar/Group-Project-1">
+                github repo
+              </a>
+            </div>
+          </div>
+          <div className="project-item">
+            <a href="https://github.com/project-2">
+              <img
+                src={WeatherDashboardImage}
+                alt="Project 2"
+                style={{ width: "200px", height: "200px" }}
+              />
+            </a>
+            <div className="project-text">
+              <p>Weather Dashboard</p>
+              <a href="https://github.com/cameragrammar/weather-dashboard">
+                github repo
+              </a>
+            </div>
+          </div>
+          <div className="project-item">
+            <a href="https://github.com/project-3">
+              <img
+                src={NoteTakerImage}
+                alt="Project 3"
+                style={{ width: "200px", height: "200px" }}
+              />
+            </a>
+            <div className="project-text">
+              <p>Express.js Note Taker</p>
+              <a href="https://github.com/cameragrammar/Note-Taker-App">
+                github repo
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
